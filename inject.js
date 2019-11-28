106620b272a6688ad62aaef7d86c54a8a0637a59
arnoudkooi <email@arnoudkooi.com>
2019-11-28 20:25:31 +0100

    Links to get out of sys_ref_list view

--- inject.js
+++ inject.js
@@ -359,6 +359,7 @@ function snuSettingsAdded() {
         addSgStudioPlatformLink();
         enhanceNotFound();
         snuPaFormulaLinks();
+        newFromPopupToTab();
 
     }
 
@@ -1066,6 +1067,24 @@ function bindPaste(showIcon) {
 
     }
 }
+
+//Because we dont like creating records in a popup with sys_ref_list view
+function newFromPopupToTab() {
+
+    if (typeof jQuery == 'undefined') return; //not in studio
+
+    if (typeof g_form != 'undefined') {
+        if (g_form.getViewName() == "sys_ref_list"){
+            var newUrl = window.location.href.replace("sysparm_view=sys_ref_list","sysparm_view=default");
+            var html = "<span title='Helper links by SN Utils' style='margin-left:5px font-weight:lighter !important'><a href='"+newUrl+"' >Default view</a> | <a href='"+newUrl+"' target='_blank'>New tab</a></span>"
+            
+            jQuery('.navbar-header').after(html);
+        }
+
+    }
+}
+
+
 
 function tryPaste() {
     if (!document.execCommand('paste')) {
