e7e88ed7b6e73722bf60906b543bcae1190e2a68
Zoran Vasic <zoran.vasicmiki@gmail.com>
2022-01-30 19:18:25 +0100

    Add css and option_schema compare. Reverse comapre order so it matches sn comapre tool

--- js/monaco/compare.js
+++ js/monaco/compare.js
@@ -8,42 +8,66 @@ document.querySelectorAll("td.left[onclick]").forEach(td => {
     lnk.addEventListener('click', evt => {
         openMonaceDiff(field);
 
-    })
+    });
     td.previousSibling.append(lnk);
+});
 
-})
+document.querySelectorAll("td.left:not([onclick]):not(.spacer)").forEach(td => {
+    let field = td.id.replace('td-pulled-', '');
 
-function openMonaceDiff(field_name) {
+    if (field.includes('css')) {
+        setupForNonEditableFields('dummy\.' + field.split('.')[1] + '\.css', field.split('.')[1] + '\.css', field, td);
+    } else if (field.includes('option_schema')) {
+        setupForNonEditableFields('dummy\.' + field.split('.')[1] + '\.option_schema', field.split('.')[1] + '\.option_schema', field, td);
+    }
+});
+
+function setupForNonEditableFields(leftBody, rightBody, field, td) {
+    let lnk = document.createElement('a');
+    let linkText = document.createTextNode("[SN Utils Compare]");
+    lnk.appendChild(linkText);
+    lnk.href = "#";
+    lnk.addEventListener('click', evt => {
+        openMonaceDiff(field, {
+            leftBody: $(leftBody).value,
+            rightBody: $(rightBody).value
+        });
+    });
+    td.previousSibling.append(lnk);
+}
+
+
+function openMonaceDiff(field_name, context) {
 
 
     var event = new CustomEvent(
         "snutils-event", {
-            detail: {
-                event: "opencodediff"
-            }
+        detail: {
+            event: "opencodediff"
         }
+    }
     );
     window.top.document.dispatchEvent(event);
 
     setTimeout(function () {
-        var rightBody = gel(field_name).value;
-        var leftBody = _getElementByPrefix('pulled', field_name).value;
+        var rightBody = context ? context.rightBody : gel(field_name).value;
+        var leftBody = context ? context.leftBody : _getElementByPrefix('pulled', field_name).value;
 
         var data = {
-            rightBody: rightBody,
-            leftBody: leftBody
-        }
+            rightBody: leftBody, //reverse so left is local right remote as it is in SN
+            leftBody: rightBody
+        };
         var event = new CustomEvent(
             "snutils-event", {
-                detail: {
-                    event: "fillcodediff",
-                    command: data
-                }
+            detail: {
+                event: "fillcodediff",
+                command: data
             }
+        }
         );
         window.top.document.dispatchEvent(event);
 
-    }, 400)
+    }, 400);
 
 
 }
