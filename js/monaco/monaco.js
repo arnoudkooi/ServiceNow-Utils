ad7770493d9efad77beb8f97a2e78a79be82b201
Jace <638764+jacebenson@users.noreply.github.com>
2022-09-02 12:21:20 -0500

    Add cancel my transaction to background scripts running counter

--- js/monaco/monaco.js
+++ js/monaco/monaco.js
@@ -224,6 +224,6 @@ function startStopWatch() {
 	var startTime = Date.now();
 	timerInterval = setInterval(function() {
 		var elapsedTime = Date.now() - startTime;
-		result.innerHTML = (elapsedTime / 1000).toFixed(3) + ' - Background script running... <hr />';
+		result.innerHTML = (elapsedTime / 1000).toFixed(3) + ' - Background script running... <a href="/cancel_my_transaction.do">Cancel my transaction</a><hr />';
 	}, 100);
 }
